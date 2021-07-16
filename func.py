4ff34656731c9b81a32f08fd7c177655cd0284af
HC-kang <weston0713@gmail.com>
2021-07-16 17:59:52 +0900

    0716

--- func.py
+++ func.py
@@ -18,3 +18,18 @@ def softmax(x):
 def relu(x):
     return np.maximum(0, x)
 
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t*np.log(y+delta))
+
+def cross_entropy_error(y, t):
+    if y.dim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t*np.log(y + 1e-7)) / batch_size
+
